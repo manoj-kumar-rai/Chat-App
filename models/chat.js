0f3ce320ccf9e637abb56812af44d1bf057c750b
manoj-kumar-rai <iammanojkumarrai@gmail.com>
2026-03-21 23:48:17 +0530

    Created Structure

---
+++ models/chat.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+    from: {
+        type: String,
+        required: true
+    },
+    to: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String
+    },
+    created_at: {
+        type: Date,
+        required: true
+    }
+})
