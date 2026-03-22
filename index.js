225c14492779552b3991e5036e65c0ff3514279f
manoj-kumar-rai <iammanojkumarrai@gmail.com>
2026-03-22 16:28:32 +0530

    Sending chat is updated

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Chat = require("./models/chat.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}))
 
 
 main()
@@ -26,10 +27,31 @@ app.get("/chats", async (req, res) => {
     res.render("index.ejs", {chats});
 })
 
+//New Route
+app.get("/chats/new", (req, res) => {
+    res.render("new.ejs")
+})
+
 let PORT = 8080;
 
 app.get("/", (req, res) => {
     res.send("Home Route");    
+})
+
+//Create Route
+app.post("/chats", (req, res) => {
+    let {from, to, message} = req.body
+    let newChat = new Chat({
+        from : from,
+        to : to,
+        message : message,
+        created_at : new Date()
+    });
+    newChat.save()
+    .then((res) => {console.log("Chat was saved");
+    }).catch((err) => {console.log(err);
+    })
+    res.redirect("/chats")
 })
 
 app.listen(PORT, () => {
